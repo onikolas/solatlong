53ed9ed8b85dad0c3a9da181ab492998d0774d2d
nikolas <onikolas@nikolas-sager.(none)>
2013-02-13 11:09:47 +0200

    Envmaker now works correctly. The lens model is currently built in (equisolid). It should be easy to support more lenses later if needed.

--- src/envmaker.cpp
+++ src/envmaker.cpp
@@ -9,29 +9,39 @@ const float PI = 3.14159265358979323846;
 
 int main(int argc, char** argv)
 {
-	if(argc < 3)
+	if(argc < 3 )
 	{
 		cout << "  Create a latitude longitude HDR environemnt map from a fisheye HDR image \n";
-		cout << "  Usage: envmaker [fisheye image] [diameter] [camera f] [center x] [center y] \n\n";
-		cout << "  The image size of the lat-long image is dirived from the diameter\n";
+		cout << "  Usage: envmaker [fisheye image] [diameter] [focal length] [center x] [center y] [size x] [size y] [latlong image]\n\n";
+		cout << "  [fisheye image]    Image to process\n";
+		cout << "  [diameter]         Diameter of the image circle in pixels\n";
+		cout << "  [focal length]     The focal length. Enter -1 if not known\n";
+		cout << "  [center x]         Center of the image circle\n";
+		cout << "  [center y]         \n";
+		cout << "  [size x]           Size of the latlong to be created\n";
+		cout << "  [size y]           \n";
+		cout << "  [latlong image]    Filename of the latlong image\n\n";
+		cout << "  Note: The diameter is ignored if the focal length is provided\n";
 		return -1;
 	}
 
 	int centerx, centery;
+	int latlong_w, latlong_h;
 	int diameter = atoi(argv[2]);
 	float camera_f = atof(argv[3]);
+
+	if(camera_f == -1)
+	{
+		cout << "Finding the focal length from the diameter not yet implemented\n";
+		return -1;
+	}
 
 	//read the fisheye exr
 	fRGB *fisheye, *latlong;
 	int w,h;
 	ReadEXR(argv[1], 1, fisheye, w, h);
 
-	//TODO: change size depending on the diameter
-	int latlong_w = w;
-	int latlong_h = h;
-	latlong = new fRGB[latlong_w*latlong_h]; //this is ofr testing only
-
-	if(argc == 6)
+	if(argc >= 6)
 	{
 		centerx  = atoi(argv[4]);
 		centery  = atoi(argv[5]);
@@ -41,6 +51,18 @@ int main(int argc, char** argv)
 		centerx = w/2;
 		centery = h/2;
 	}
+	if(argc >=8)
+	{
+		latlong_w = atoi(argv[6]);
+		latlong_h = atoi(argv[7]);
+	}
+	else
+	{
+		latlong_w = w;
+		latlong_h = h;
+	}
+
+	latlong = new fRGB[latlong_w*latlong_h];
 
 	//initialize to black
 	for(int i=0; i<w*h; i++)
@@ -51,33 +73,39 @@ int main(int argc, char** argv)
 	{
 		for(int j=0; j<latlong_w; j++)
 		{
-			//find pixel on fisheye image corresponding to i,j (theta,phi)
-
-			//To test i will asume a perfect semisphear, later i will use someting like the following:
-
-			//Using mapping function R = 2*f*sin(theta/2) until i know better
-			/*
-			float R = 2 * f * sin(i*PI/(2*latlong_h));
-			float maxR = 2 * f *sin(PI/4); //max theta = PI/2
-			int pixelR = (R/maxR) * (diameter/2);
-			*/
-
 			float theta = (i*PI)/latlong_h;
 			float phi = (j*2*PI)/latlong_w;
 
-			int x = (diameter/2) * sin(theta) * cos(phi) + centerx;
-			int y = (diameter/2) * sin(theta) * sin(phi) + centery;
+			//find pixel on fisheye image corresponding to i,j (theta,phi)
+
+			//Using mapping function R = 2*f*sin(theta/2) (equisolid) for Sigma 4.5mm F2.8 EX DC HSM Circular Fisheye
+			//Other lenses must be added later and selected as option
+
+			float R = 2 * camera_f * sin(theta/2); //this R is in mm because f is in mm
+			float maxR = 2 * camera_f * sin(PI/4); //max theta = 90
+			int pixelR = (R/maxR) * diameter/2;    //R in pixels
+
+			int x = pixelR * cos(phi) + centerx;
+			int y = pixelR * sin(phi) + centery;
+
+			//asuming that the image is a perfect semisphere (equisolid is ?)
+			//int x = (diameter/2) * sin(theta) * cos(phi) + centerx;
+			//int y = (diameter/2) * sin(theta) * sin(phi) + centery;
 
 			latlong[i*latlong_w+j] = fisheye[y*w+x];
 		}
 	}
-
-	//add .latlong to the input filename
-	string filename = argv[1];
-	int dot = filename.find(".exr");
-	filename = filename.substr(0, dot);
-	filename += ".latlong.exr";
-
+	string filename;
+	if(argc == 9)
+		filename = argv[8];
+	else
+	{
+		//add .latlong to the input filename
+		filename= argv[1];
+		int dot = filename.find(".exr");
+		filename = filename.substr(0, dot);
+		filename += ".latlong.exr";
+	}
 	WriteEXR(filename, latlong, w, h);
 
 	return 0;
