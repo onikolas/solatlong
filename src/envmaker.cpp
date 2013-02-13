c9a78d9f8a4d8961147b2ad9c1f375db0a91f49f
nikolas <onikolas@nikolas-sager.(none)>
2013-02-13 15:12:02 +0200

    some fixes

--- src/envmaker.cpp
+++ src/envmaker.cpp
@@ -9,17 +9,16 @@ const float PI = 3.14159265358979323846;
 
 int main(int argc, char** argv)
 {
-	if(argc < 3 )
+	if(argc < 4)
 	{
-		cout << "  Create a latitude longitude HDR environemnt map from a fisheye HDR image \n";
-		cout << "  Usage: envmaker [fisheye image] [diameter] [focal length] [center x] [center y] [size x] [size y] [latlong image]\n\n";
+		cout << "\n  Create a latitude longitude HDR environemnt map from a fisheye HDR image \n";
+		cout << "  Usage: envmaker [fisheye image] [diameter] [focal length] [center x] [center y] [width] [latlong image]\n\n";
 		cout << "  [fisheye image]    Image to process\n";
 		cout << "  [diameter]         Diameter of the image circle in pixels\n";
 		cout << "  [focal length]     The focal length. Enter -1 if not known\n";
 		cout << "  [center x]         Center of the image circle\n";
 		cout << "  [center y]         \n";
-		cout << "  [size x]           Size of the latlong to be created\n";
-		cout << "  [size y]           \n";
+		cout << "  [width]            Width of the latlong to be created. Height is set to width/2)\n";
 		cout << "  [latlong image]    Filename of the latlong image\n\n";
 		cout << "  Note: The diameter is ignored if the focal length is provided\n";
 		return -1;
@@ -51,25 +50,25 @@ int main(int argc, char** argv)
 		centerx = w/2;
 		centery = h/2;
 	}
-	if(argc >=8)
+	if(argc >=7)
 	{
 		latlong_w = atoi(argv[6]);
-		latlong_h = atoi(argv[7]);
+		latlong_h = latlong_w/2;
 	}
 	else
 	{
 		latlong_w = w;
-		latlong_h = h;
+		latlong_h = latlong_w/2;
 	}
 
 	latlong = new fRGB[latlong_w*latlong_h];
 
 	//initialize to black
-	for(int i=0; i<w*h; i++)
+	for(int i=0; i<latlong_w*latlong_h; i++)
 		latlong[i] = fRGB(0,0,0);
 
 	//go over the pixels of the new image
-	for(int i=0 ; i<latlong_h; i++)
+	for(int i=0 ; i<=latlong_h/2; i++)
 	{
 		for(int j=0; j<latlong_w; j++)
 		{
@@ -79,7 +78,7 @@ int main(int argc, char** argv)
 			//find pixel on fisheye image corresponding to i,j (theta,phi)
 
 			//Using mapping function R = 2*f*sin(theta/2) (equisolid) for Sigma 4.5mm F2.8 EX DC HSM Circular Fisheye
-			//Other lenses must be added later and selected as option
+			//Other lenses can be added later if needed
 
 			float R = 2 * camera_f * sin(theta/2); //this R is in mm because f is in mm
 			float maxR = 2 * camera_f * sin(PI/4); //max theta = 90
@@ -96,8 +95,8 @@ int main(int argc, char** argv)
 		}
 	}
 	string filename;
-	if(argc == 9)
-		filename = argv[8];
+	if(argc == 8)
+		filename = argv[7];
 	else
 	{
 		//add .latlong to the input filename
@@ -106,7 +105,7 @@ int main(int argc, char** argv)
 		filename = filename.substr(0, dot);
 		filename += ".latlong.exr";
 	}
-	WriteEXR(filename, latlong, w, h);
+	WriteEXR(filename, latlong, latlong_w, latlong_h);
 
 	return 0;
 }
